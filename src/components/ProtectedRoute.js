405fb771c409faf28a8fc7b5c9784f9c3ae2dcf0
akash13997 <akash.singh13997@gmail.com>
2024-09-19 13:08:42 +0530

    sd

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { useUserAuth } from "../context/UserAuthContext";
+
 const ProtectedRoute = ({ children }) => {
   const { user } = useUserAuth();
-  if (!user) {
+  const [isRedirecting, setIsRedirecting] = useState(false);
+
+  useEffect(() => {
+    if (!user) {
+      alert("You must be logged in to view this page.");
+      setIsRedirecting(true);
+    }
+  }, [user]);
+
+  if (!user && isRedirecting) {
     return <Navigate to="/" />;
   }
-  return children;
+
+  return user ? children : null;
 };
 
 export default ProtectedRoute;
