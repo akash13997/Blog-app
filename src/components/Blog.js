3a69d12c66f00f131dfe04195605e2d975ef80c9
Akash Singh <akash@delimp.com>
2026-05-31 19:04:03 +0530

    fix aliment

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -92,144 +92,144 @@ const Blog = () => {
     <div className="container py-4">
 
       <div className="row justify-content-center">
-  <div className="col-12 col-md-8 col-lg-6">
-    <div className="card shadow-sm">
-      <div className="card-body">
-        <h2 className="mb-4 text-center">Add New Blog</h2>
+        <div className="col-12 col-md-8 col-lg-6">
+          <div className="card shadow-sm">
+            <div className="card-body">
+              <h2 className="mb-4 text-center">Add New Blog</h2>
 
-        <input
-          type="text"
-          className="form-control mb-3"
-          name="title"
-          value={newPost.title}
-          placeholder="Enter blog title"
-          onChange={handleNewPostChange}
-        />
+              <input
+                type="text"
+                className="form-control mb-3"
+                name="title"
+                value={newPost.title}
+                placeholder="Enter blog title"
+                onChange={handleNewPostChange}
+              />
 
-        <textarea
-          className="form-control mb-3"
-          rows="4"
-          name="description"
-          value={newPost.description}
-          placeholder="Enter blog description"
-          onChange={handleNewPostChange}
-        />
+              <textarea
+                className="form-control mb-3"
+                rows="4"
+                name="description"
+                value={newPost.description}
+                placeholder="Enter blog description"
+                onChange={handleNewPostChange}
+              />
 
-        <input
-          type="file"
-          className="form-control mb-3"
-          onChange={handleImageUpload}
-        />
+              <input
+                type="file"
+                className="form-control mb-3"
+                onChange={handleImageUpload}
+              />
 
-        {newPost.image && (
-          <img
-            src={newPost.image}
-            alt="Preview"
-            className="img-fluid rounded mb-3"
-          />
-        )}
+              {newPost.image && (
+                <img
+                  src={newPost.image}
+                  alt="Preview"
+                  className="img-fluid rounded mb-3"
+                />
+              )}
 
-        <button
-          className="btn btn-primary w-100"
-          onClick={addPost}
-        >
-          Add Post
-        </button>
+              <button
+                className="btn btn-primary w-100"
+                onClick={addPost}
+              >
+                Add Post
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</div>
 
       <h3>All Blog Posts</h3>
       {posts.length === 0 ? (
         <p>No posts available.</p>
       ) : (
         <div className="row">
-  {posts.map((post) => (
-    <div key={post.id} className="col-12 col-md-6 col-lg-4 mb-4">
-      <div className="card h-100 shadow-sm">
-        {post.image && (
-          <img
-            src={post.image}
-            alt="Cover"
-            className="card-img-top img-fluid"
-            style={{
-              height: "200px",
-              objectFit: "cover",
-            }}
-          />
-        )}
+        {posts.map((post) => (
+          <div key={post.id} className="col-12 col-md-6 col-lg-4 mb-4">
+            <div className="card h-100 shadow-sm">
+              {post.image && (
+                <img
+                  src={post.image}
+                  alt="Cover"
+                  className="card-img-top img-fluid"
+                  style={{
+                    height: "200px",
+                    objectFit: "cover",
+                  }}
+                />
+              )}
 
-        <div className="card-body d-flex flex-column">
-          {editingPost === post.id ? (
-            <>
-              <input
-                type="text"
-                className="form-control mb-2"
-                value={updatedPost.title}
-                onChange={(e) =>
-                  setUpdatedPost({
-                    ...updatedPost,
-                    title: e.target.value,
-                  })
-                }
-              />
+              <div className="card-body d-flex flex-column">
+                {editingPost === post.id ? (
+                  <>
+                    <input
+                      type="text"
+                      className="form-control mb-2"
+                      value={updatedPost.title}
+                      onChange={(e) =>
+                        setUpdatedPost({
+                          ...updatedPost,
+                          title: e.target.value,
+                        })
+                      }
+                    />
 
-              <textarea
-                className="form-control mb-2"
-                rows="4"
-                value={updatedPost.description}
-                onChange={(e) =>
-                  setUpdatedPost({
-                    ...updatedPost,
-                    description: e.target.value,
-                  })
-                }
-              />
+                    <textarea
+                      className="form-control mb-2"
+                      rows="4"
+                      value={updatedPost.description}
+                      onChange={(e) =>
+                        setUpdatedPost({
+                          ...updatedPost,
+                          description: e.target.value,
+                        })
+                      }
+                    />
 
-              <input
-                type="file"
-                className="form-control mb-3"
-                onChange={handleUpdateImageUpload}
-              />
+                    <input
+                      type="file"
+                      className="form-control mb-3"
+                      onChange={handleUpdateImageUpload}
+                    />
 
-              <button
-                className="btn btn-success w-100"
-                onClick={() => updatePost(post.id)}
-              >
-                Save
-              </button>
-            </>
-          ) : (
-            <>
-              <h5 className="card-title">{post.title}</h5>
+                    <button
+                      className="btn btn-success w-100"
+                      onClick={() => updatePost(post.id)}
+                    >
+                      Save
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <h5 className="card-title">{post.title}</h5>
 
-              <p className="card-text flex-grow-1">
-                {post.description}
-              </p>
+                    <p className="card-text flex-grow-1">
+                      {post.description}
+                    </p>
 
-              <div className="d-flex flex-column flex-sm-row gap-2">
-                <button
-                  className="btn btn-warning flex-fill mb-2 mb-sm-0"
-                  onClick={() => startEditingPost(post)}
-                >
-                  Edit
-                </button>
+                    <div className="d-flex flex-column flex-sm-row gap-2">
+                      <button
+                        className="btn btn-warning flex-fill mb-2 mb-sm-0"
+                        onClick={() => startEditingPost(post)}
+                      >
+                        Edit
+                      </button>
 
-                <button
-                  className="btn btn-danger flex-fill"
-                  onClick={() => deletePost(post.id)}
-                >
-                  Delete
-                </button>
+                      <button
+                        className="btn btn-danger flex-fill"
+                        onClick={() => deletePost(post.id)}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </>
+                )}
               </div>
-            </>
-          )}
-        </div>
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
-  ))}
-</div>
       )}
     </div>
   );
